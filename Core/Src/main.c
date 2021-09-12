f66e490241d39fc7925d6ee7955538fb1ed749b8
nguyenhoang2001 <75627141+nguyenhoang2001@users.noreply.github.com>
2021-09-12 15:30:03 +0700

    done ex1

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -86,7 +86,34 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   /* USER CODE BEGIN 2 */
-  void display7SEG(int num) {}
+  while (1)
+    {
+  // Turn on the RED LED and turn off the YELLOW LED
+  	  HAL_GPIO_WritePin(LED_RED_GPIO_Port, LED_RED_Pin, 0);
+  	  HAL_GPIO_WritePin(LED_YELLOW_GPIO_Port, LED_YELLOW_Pin, GPIO_PIN_SET);
+  // Delay for 2 seconds
+  	  HAL_Delay(2000);
+  // Turn on the YELLOW LED and turn off the RED LED
+  	  HAL_GPIO_WritePin(LED_RED_GPIO_Port, LED_RED_Pin, 1);
+  	  HAL_GPIO_WritePin(LED_YELLOW_GPIO_Port, LED_YELLOW_Pin, GPIO_PIN_RESET);
+  // Delay for 2 seconds
+  	  HAL_Delay(2000);
+      /* USER CODE END WHILE */
+
+      /* USER CODE BEGIN 3 */
+    }
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+    {
+
+
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+    }
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -150,13 +177,10 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOA_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOA, LED_LEFT_RED_Pin|LED_LEFT_YELLOW_Pin|LED_LEFT_GREEN_Pin|LED_RIGHT_RED_Pin
-                          |LED_RIGHT_YELLOW_Pin|LED_RIGHT_GREEN_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOA, LED_RED_Pin|LED_YELLOW_Pin, GPIO_PIN_RESET);
 
-  /*Configure GPIO pins : LED_LEFT_RED_Pin LED_LEFT_YELLOW_Pin LED_LEFT_GREEN_Pin LED_RIGHT_RED_Pin
-                           LED_RIGHT_YELLOW_Pin LED_RIGHT_GREEN_Pin */
-  GPIO_InitStruct.Pin = LED_LEFT_RED_Pin|LED_LEFT_YELLOW_Pin|LED_LEFT_GREEN_Pin|LED_RIGHT_RED_Pin
-                          |LED_RIGHT_YELLOW_Pin|LED_RIGHT_GREEN_Pin;
+  /*Configure GPIO pins : LED_RED_Pin LED_YELLOW_Pin */
+  GPIO_InitStruct.Pin = LED_RED_Pin|LED_YELLOW_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
