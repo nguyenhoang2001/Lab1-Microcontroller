5ef08484088d8c8ad4690b9fe40b92aa5995c022
nguyenhoang2001 <75627141+nguyenhoang2001@users.noreply.github.com>
2021-09-12 16:45:43 +0700

    ex3

--- Core/Inc/main.h
+++ Core/Inc/main.h
@@ -58,6 +58,18 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
+#define LED_LEFT_RED_Pin GPIO_PIN_1
+#define LED_LEFT_RED_GPIO_Port GPIOA
+#define LED_LEFT_YELLOW_Pin GPIO_PIN_2
+#define LED_LEFT_YELLOW_GPIO_Port GPIOA
+#define LED_LEFT_GREEN_Pin GPIO_PIN_3
+#define LED_LEFT_GREEN_GPIO_Port GPIOA
+#define LED_RIGHT_RED_Pin GPIO_PIN_5
+#define LED_RIGHT_RED_GPIO_Port GPIOA
+#define LED_RIGHT_YELLOW_Pin GPIO_PIN_6
+#define LED_RIGHT_YELLOW_GPIO_Port GPIOA
+#define LED_RIGHT_GREEN_Pin GPIO_PIN_7
+#define LED_RIGHT_GREEN_GPIO_Port GPIOA
 /* USER CODE BEGIN Private defines */
 
 /* USER CODE END Private defines */
--- Core/Src/main.c
+++ Core/Src/main.c
@@ -86,19 +86,38 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   /* USER CODE BEGIN 2 */
-  void display7SEG(int num) {}
+
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
   while (1)
     {
+	  HAL_GPIO_WritePin(LED_RIGHT_RED_GPIO_Port, LED_RIGHT_RED_Pin, 1);
+	  	  HAL_GPIO_WritePin(LED_LEFT_GREEN_GPIO_Port, LED_LEFT_GREEN_Pin, 1);
+	  	  HAL_Delay(3000);
+
+	  	  HAL_GPIO_WritePin(LED_LEFT_YELLOW_GPIO_Port, LED_LEFT_YELLOW_Pin, 1);
+	  	  HAL_GPIO_WritePin(LED_LEFT_GREEN_GPIO_Port, LED_LEFT_GREEN_Pin, 0);
+	  	  HAL_Delay(2000);
+
+	  	  HAL_GPIO_WritePin(LED_LEFT_RED_GPIO_Port, LED_LEFT_RED_Pin, 1);
+	  	  HAL_GPIO_WritePin(LED_LEFT_YELLOW_GPIO_Port, LED_LEFT_YELLOW_Pin, 0);
+
+	  	  HAL_GPIO_WritePin(LED_RIGHT_RED_GPIO_Port, LED_RIGHT_RED_Pin, 0);
+	  	  HAL_GPIO_WritePin(LED_RIGHT_GREEN_GPIO_Port, LED_RIGHT_GREEN_Pin, 1);
+	  	  HAL_Delay(3000);
+
+	  	  HAL_GPIO_WritePin(LED_RIGHT_GREEN_GPIO_Port, LED_RIGHT_GREEN_Pin, 0);
+	  	  HAL_GPIO_WritePin(LED_RIGHT_YELLOW_GPIO_Port, LED_RIGHT_YELLOW_Pin, 1);
+	  	  HAL_Delay(2000);
+	  	  HAL_GPIO_WritePin(LED_RIGHT_YELLOW_GPIO_Port, LED_RIGHT_YELLOW_Pin, 0);
+	  	  HAL_GPIO_WritePin(LED_LEFT_RED_GPIO_Port, LED_LEFT_RED_Pin, 0);
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
-    }
-  /* USER CODE END 2 */
-
+  }
+  /* USER CODE END 3 */
 }
 
 /**
@@ -146,11 +165,25 @@ static void MX_GPIO_Init(void)
   GPIO_InitTypeDef GPIO_InitStruct = {0};
 
   /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOA_CLK_ENABLE();
   __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOA, LED_LEFT_RED_Pin|LED_LEFT_YELLOW_Pin|LED_LEFT_GREEN_Pin|LED_RIGHT_RED_Pin
+                          |LED_RIGHT_YELLOW_Pin|LED_RIGHT_GREEN_Pin, GPIO_PIN_RESET);
 
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3
                           |GPIO_PIN_4|GPIO_PIN_5|GPIO_PIN_6, GPIO_PIN_RESET);
+
+  /*Configure GPIO pins : LED_LEFT_RED_Pin LED_LEFT_YELLOW_Pin LED_LEFT_GREEN_Pin LED_RIGHT_RED_Pin
+                           LED_RIGHT_YELLOW_Pin LED_RIGHT_GREEN_Pin */
+  GPIO_InitStruct.Pin = LED_LEFT_RED_Pin|LED_LEFT_YELLOW_Pin|LED_LEFT_GREEN_Pin|LED_RIGHT_RED_Pin
+                          |LED_RIGHT_YELLOW_Pin|LED_RIGHT_GREEN_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
   /*Configure GPIO pins : PB0 PB1 PB2 PB3
                            PB4 PB5 PB6 */
